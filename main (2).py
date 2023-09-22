b43cf57ab00bac12979fd6ef9c81fa57df28c5db
asbru5h3039 <144205523+asbru5h3039@users.noreply.github.com>
2023-09-22 13:56:35 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,14 +1,42 @@
-def linear_search_product(product_list, target_product):
-    indices = []
+#Implement a class called BankAccount that represents a bank account. The class should have privateattributes for account number, account holder name, and account balance. Include methods todeposit money, withdraw money, and display the account balance. Ensure that the account balancecannot be accessed directly from outside the class. Write a program to create an instance of theBankAccount class and test the deposit and withdrawal functionality.'''
 
-    for index, product in enumerate(product_list):
-        if product == target_product:
-            indices.append(index)
 
-    return indices
+class BankAccount:
 
-# Example usage:
-products = ["apple", "banana", "apple", "orange", "apple"]
-target = "apple"
-result = linear_search_product(products, target)
-print(result)  # Output: [0, 2, 4]
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. New balance: ₹{}".format(amount,
+                                                     self.__account_balance))
+    else:
+      print("Invalid deposit amount. Please deposit a positive amount.")
+
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdrew ₹{}. New balance: ₹{}".format(amount,
+                                                    self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): ₹{}".format(
+        self.__account_holder_name, self.__account_number,
+        self.__account_balance))
+
+
+# Create an instance of the BankAccount class
+account = BankAccount(account_number="123456789",
+                      account_holder_name="Hari Prabu",
+                      initial_balance=5000.0)
+
+# Test deposit and withdrawal functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.display_balance()
